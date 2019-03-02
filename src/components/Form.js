84a694e87bd0dfb77794118c92b826becf1b775a
Cdelatorre <carlosdelatorrediaz@gmail.com>
2019-03-02 17:23:26 +0100

    feat: iteration 2 completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 
 export default class Form extends Component {
+
+  constructor (props){
+    super(props);
+
+    this.state = {
+      email : '', 
+      errors : {
+        email : false
+      }
+    }
+  }
+
+  onChange = (e) => {
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    this.setState({
+      email : e.target.value,
+      errors : {
+        ...this.state.errors,
+        email : filter.test(e.target.value)
+      }
+    })
+    console.log(this.state.errors.email)
+  }
+
   render = () => (
     <div className="Form">
       <div class="field">
         <label class="label">Username</label>
         <div class="control has-icons-left has-icons-right">
-          <input class="input is-success" type="text" placeholder="Text input" value="bulma" />
+          <input class="input is-success" type="text" placeholder="Text input" 
+          value={this.state.email} 
+          onChange={this.onChange}
+          />
           <span class="icon is-small is-left">
             <i class="fas fa-user"></i>
           </span>
@@ -15,8 +42,13 @@ export default class Form extends Component {
             <i class="fas fa-check"></i>
           </span>
         </div>
-        <p class="help is-success">This username is available</p>
-        <p class="help is-danger">This email is invalid</p>
+        { 
+          <p class="help is-success">This username is available</p>
+        }
+        { !this.state.errors.email &&
+           <p class="help is-danger">This email is invalid</p>
+        }
+       
       </div>
 
       <div class="control">
